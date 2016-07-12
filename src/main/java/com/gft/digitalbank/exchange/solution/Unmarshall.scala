46134824e5e69bc221f2a63a14bafe842bc6ac52
speedcom <mateusz.maciaszekhpc@gmail.com>
2016-07-12 19:37:08 +0200

    unmarshall text-messages to broker-message classes instances

--- src/main/java/com/gft/digitalbank/exchange/solution/Unmarshall.scala
+++ src/main/java/com/gft/digitalbank/exchange/solution/Unmarshall.scala
@@ -1,40 +1,73 @@
 package com.gft.digitalbank.exchange.solution
 
+import javax.jms.TextMessage
+
+import com.gft.digitalbank.exchange.model.OrderDetails
+import com.gft.digitalbank.exchange.model.orders._
 import spray.json.DefaultJsonProtocol._
 import spray.json._
 
-import scala.concurrent.ExecutionContext
+import scala.util.{Failure, Try}
+
 object Unmarshall {
 
-  val messageTypeField = "messageType"
+  def unapply(txt: TextMessage): Try[BrokerMessage] = {
+    val json = txt.getText.parseJson.asJsObject
 
-  private[this] implicit val detailsReader: JsonFormat[Details] = jsonFormat2(Details.apply)
-  private[this] implicit val buyReader: JsonReader[Buy]         = jsonFormat6(Buy.apply)
-  private[this] implicit val sellReader: JsonReader[Sell]       = jsonFormat6(Sell.apply)
-  private[this] implicit val modifyReader: JsonReader[Modify]   = jsonFormat5(Modify.apply)
-  private[this] implicit val cancelReader: JsonReader[Cancel]   = jsonFormat4(Cancel.apply)
-
-  def jsonToJsonCommand(jsObject: JsObject): Either[ShutdownNotification, OrderCommand] = {
-    val commandType = jsObject.fields.getOrElse(messageTypeField, throw new IllegalArgumentException(s"Message doesn't contain field '$messageTypeField"))
-    commandType match {
-      case JsString("SHUTDOWN_NOTIFICATION") => Left(ShutdownNotification(jsObject.fields("broker").convertTo[String]))
-      case _                                 => Right(jsonToOrderCommand(jsObject))
+    json.getFields("messageType").headOption match {
+      case Some(JsString("ORDER"))                 => Try(json2PositionOrder(json))
+      case Some(JsString("MODIFICATION"))          => Try(json2ModificationOrder(json))
+      case Some(JsString("CANCEL"))                => Try(json2CancellationOrder(json))
+      case Some(JsString("SHUTDOWN_NOTIFICATION")) => Try(json2ShutdownNotification(json))
+      case other                                   => Failure(new IllegalArgumentException(s"Message ${txt.getText} doesn't contain field messageType"))
     }
   }
 
   @inline
-  final private[this] def jsonToOrderCommand(jsObject: JsObject): OrderCommand = {
-    val commandType = jsObject.fields(messageTypeField)
+  private def json2PositionOrder(json: JsObject): PositionOrder = json.getFields("messageType", "side", "id", "timestamp", "broker", "client", "product", "details") match {
+    case Seq(JsString(_), JsString(side), JsNumber(id), JsNumber(timestamp), JsString(broker), JsString(client), JsString(product), Seq(JsNumber(amount), JsNumber(price))) =>
+      new PositionOrder.PositionOrderBuilder()
+        .timestamp(timestamp.toLong)
+        .id(id.toInt)
+        .broker(broker)
+        .client(client)
+        .product(product)
+        .side(if(side == "BUY") Side.BUY else Side.SELL)
+        .details(new OrderDetails(amount.toInt, price.toInt))
+        .build()
+  }
 
-    commandType match {
-      case JsString("ORDER") =>
-        val side = jsObject.fields("side").convertTo[String]
-        if (side == "BUY") {
-          jsObject.convertTo[Buy]
-        } else {
-          jsObject.convertTo[Sell]
-        }
-      case _ => println(jsObject); ???
-    }
+  @inline
+  private def json2CancellationOrder(json: JsObject): CancellationOrder = json.getFields("messageType", "id", "timestamp", "broker", "cancelledOrderId") match {
+    case Seq(JsString(_), JsNumber(id), JsNumber(timestamp), JsString(broker), JsNumber(cancelledOrderId)) =>
+      new CancellationOrder.CancellationOrderBuilder()
+        .messageType(MessageType.CANCEL)
+        .timestamp(timestamp.toLong)
+        .broker(broker)
+        .cancelledOrderId(cancelledOrderId.toInt)
+        .id(id.toInt)
+        .build()
+  }
+
+  @inline
+  private def json2ShutdownNotification(json: JsObject): ShutdownNotification = json.getFields("messageType", "timestamp", "id", "broker") match {
+    case Seq(JsString(_), JsNumber(timestamp), JsNumber(id), JsString(broker)) =>
+      new ShutdownNotification.ShutdownNotificationBuilder()
+        .timestamp(timestamp.toLong)
+        .broker(broker)
+        .id(id.toInt)
+        .build()
+  }
+
+  @inline
+  private def json2ModificationOrder(json: JsObject): ModificationOrder = json.getFields("messageType", "id", "timestamp", "broker", "modifiedOrderId", "details") match {
+    case Seq(JsString(_), JsNumber(id), JsNumber(timestamp), JsString(broker), JsNumber(modifiedOrderId), Seq(JsNumber(amount), JsNumber(price))) =>
+      new ModificationOrder.ModificationOrderBuilder()
+        .timestamp(timestamp.toLong)
+        .id(id.toInt)
+        .broker(broker)
+        .modifiedOrderId(modifiedOrderId.toInt)
+        .details(new OrderDetails(amount.toInt, price.toInt))
+        .build()
   }
 }
