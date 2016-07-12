2709e32f722a7989130002222259ae997b26dd60
speedcom <mateusz.maciaszekhpc@gmail.com>
2016-07-12 20:50:27 +0200

    consumer actor

--- src/main/java/com/gft/digitalbank/exchange/solution/ConsumerActor.scala
+++ src/main/java/com/gft/digitalbank/exchange/solution/ConsumerActor.scala
@@ -1,37 +1,29 @@
 package com.gft.digitalbank.exchange.solution
 
-import javax.jms.{ Message, MessageConsumer, MessageListener, TextMessage }
-
-import akka.actor.{ Actor, ActorRef }
-import spray.json._
-
-import scala.util.Try
+import javax.jms.{Message, MessageConsumer, MessageListener, TextMessage}
+import akka.actor.{Actor, ActorRef}
+import com.gft.digitalbank.exchange.model.orders.{CancellationOrder, ModificationOrder, PositionOrder, ShutdownNotification}
+import scala.util.{Failure, Success}
 
 class ConsumerActor(messageConsumer: MessageConsumer, destination: String, exchangeActorRef: ActorRef) extends Actor {
 
-  messageConsumer.setMessageListener(
-      new MessageListener {
-    override def onMessage(message: Message): Unit = message match {
-      case txt: TextMessage =>
-        val json = txt.getText.parseJson.asJsObject
-        self.tell(json, ActorRef.noSender)
-      case other =>
-        throw new IllegalArgumentException(s"Received non-TextMessage from ActiveMQ: $other")
-    }
+  messageConsumer.setMessageListener(new MessageListener {
+    override def onMessage(message: Message): Unit = self.tell(message, ActorRef.noSender)
   })
 
   override def receive: Receive = {
-    case json: JsObject =>
-      Try {
-        val value = Unmarshall.jsonToJsonCommand(json)
-        println(s"Decoded $json as $value")
-        value match {
-          case Left(ShutdownNotification(broker)) =>
-            exchangeActorRef ! BrokerStopped(broker.split("-").last)
-            messageConsumer.close()
-            context.stop(self)
-          case Right(orderCommand) => exchangeActorRef ! ProcessMessage(orderCommand)
-        }
-      }.failed.foreach(_.printStackTrace())
+    case txt: TextMessage =>
+      Unmarshaller.unapply(txt) match {
+        case Success(po: PositionOrder)        => ???
+        case Success(co: CancellationOrder)    => ???
+        case Success(mo: ModificationOrder)    => ???
+        case Success(sn: ShutdownNotification) =>
+          exchangeActorRef ! BrokerStopped(sn.getBroker.split("-").last)
+          messageConsumer.close()
+          context.stop(self)
+        case Failure(msg)                      => msg.printStackTrace()
+      }
+    case other =>
+      throw new IllegalArgumentException(s"Received non-TextMessage from ActiveMQ: $other")
   }
 }
