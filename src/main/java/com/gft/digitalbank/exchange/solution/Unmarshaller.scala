f66aae9541b620d096f4c9c41142bcdf32183170
speedcom <mateusz.maciaszekhpc@gmail.com>
2016-07-13 20:06:48 +0200

    common method to deserialize json details

--- src/main/java/com/gft/digitalbank/exchange/solution/Unmarshaller.scala
+++ src/main/java/com/gft/digitalbank/exchange/solution/Unmarshaller.scala
@@ -29,17 +29,16 @@ object Unmarshaller {
 
     final def toPositionOrder: PositionOrderCommand = json.getFields("messageType", "side", "id", "timestamp", "broker", "client", "product", "details") match {
       case Seq(JsString(_), JsString(side), JsNumber(id), JsNumber(timestamp), JsString(broker), JsString(client), JsString(product), JsObject(details)) =>
-        (details("amount"), details("price")) match {
-          case (JsNumber(amount), JsNumber(price)) =>
-            PositionOrderCommand(PositionOrder.builder()
-              .timestamp(timestamp.toLong)
-              .id(id.toInt)
-              .broker(broker)
-              .client(client)
-              .product(product)
-              .side(if(side == "BUY") Side.BUY else Side.SELL)
-              .details(new OrderDetails(amount.toInt, price.toInt))
-              .build())
+        orderWithDetails(details) { (amount, price) =>
+          PositionOrderCommand(PositionOrder.builder()
+            .timestamp(timestamp.toLong)
+            .id(id.toInt)
+            .broker(broker)
+            .client(client)
+            .product(product)
+            .side(if(side == "BUY") Side.BUY else Side.SELL)
+            .details(new OrderDetails(amount, price))
+            .build())
         }
     }
 
@@ -65,17 +64,26 @@ object Unmarshaller {
 
     final def toModificationOrder: ModificationOrderCommand = json.getFields("messageType", "id", "timestamp", "broker", "modifiedOrderId", "details") match {
       case Seq(JsString(_), JsNumber(id), JsNumber(timestamp), JsString(broker), JsNumber(modifiedOrderId), JsObject(details)) =>
-        (details("amount"), details("price")) match {
-          case (JsNumber(amount), JsNumber(price)) =>
-            ModificationOrderCommand(ModificationOrder.builder()
-              .timestamp(timestamp.toLong)
-              .id(id.toInt)
-              .broker(broker)
-              .modifiedOrderId(modifiedOrderId.toInt)
-              .details(new OrderDetails(amount.toInt, price.toInt))
-              .build())
+        orderWithDetails(details) { (amount, price) =>
+          ModificationOrderCommand(ModificationOrder.builder()
+            .timestamp(timestamp.toLong)
+            .id(id.toInt)
+            .broker(broker)
+            .modifiedOrderId(modifiedOrderId.toInt)
+            .details(new OrderDetails(amount, price))
+            .build())
         }
     }
+  }
+
+  private[this] def orderWithDetails[T](details: Map[String, JsValue])(toOrder: (Int, Int) => T): T = {
+    val orderCmdOpt = for {
+      JsNumber(amount) <- details.get("amount")
+      JsNumber(price)  <- details.get("price")
+    } yield {
+      toOrder(amount.toInt, price.toInt)
+    }
+    orderCmdOpt getOrElse deserializationError("Json deserialization exception - Amount or price expected")
   }
 
 }
