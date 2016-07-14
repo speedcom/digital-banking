b76651c24563bc90615a1bba0438fabc7b75878f
speedcom <mateusz.maciaszekhpc@gmail.com>
2016-07-14 21:45:14 +0200

    use PriorityDequeue as a collection for storing buy/sell signals in OrderBook

--- src/main/java/com/gft/digitalbank/exchange/solution/OrderBookActor.scala
+++ src/main/java/com/gft/digitalbank/exchange/solution/OrderBookActor.scala
@@ -1,42 +1,41 @@
 package com.gft.digitalbank.exchange.solution
 
 import akka.actor.{Actor, ActorRef}
-import com.gft.digitalbank.exchange.model.orders.{PositionOrder, Side}
+import com.gft.digitalbank.exchange.model.orders.PositionOrder
 import com.gft.digitalbank.exchange.model.{OrderBook, Transaction}
+
+import scala.collection.mutable
 
 class OrderBookActor(exchangeActorRef: ActorRef, product: String) extends Actor {
   import OrderBookActor._
 
-  val buy  = collection.mutable.Buffer.empty[PositionOrder]
-  val sell = collection.mutable.Buffer.empty[PositionOrder]
+  private val buy  = new mutable.PriorityQueue[BuyOrderValue]()(BuyOrderValue.priceDescTimestampAscOrdering)
+  private val sell = new mutable.PriorityQueue[SellOrderValue]()(SellOrderValue.priceAscTimestampAscOrdering)
 
   override def receive: Receive = {
-    case x =>
-      println(s"BOOK $product got $x")
-      x match {
-        case GetTransactions =>
-          println(s"Processing finished, send not matched orders")
+    case GetTransactions =>
+      println(s"Processing finished, send not matched orders")
 
-          //Transactions should be sent immediately when they are matched
-          exchangeActorRef ! ExchangeActor.RecordTransaction(
-              product,
-              Transaction.builder().id(1).amount(100).price(100).product(product).brokerBuy("1").brokerSell("2").clientBuy("100").clientSell("101").build()
-          )
+      //Transactions should be sent immediately when they are matched
+      exchangeActorRef ! ExchangeActor.RecordTransaction(
+        product,
+        Transaction.builder().id(1).amount(100).price(100).product(product).brokerBuy("1").brokerSell("2").clientBuy("100").clientSell("101").build()
+      )
 
-          exchangeActorRef ! ExchangeActor.RecordOrderBook(product, OrderBook.builder().product(product).build())
+      exchangeActorRef ! ExchangeActor.RecordOrderBook(product, OrderBook.builder().product(product).build())
 
-          context.stop(self)
-        case BuyOrder(b) =>
-          buy.append(b)
-          self ! MatchTransactions
-        case SellOrder(s) =>
-          sell.append(s)
-          self ! MatchTransactions
-        case MatchTransactions =>
-          println(s"Matching $product: $buy $sell")
-      }
+      context.stop(self)
+    case BuyOrder(b) =>
+      buy enqueue BuyOrderValue(b)
+      self ! MatchTransactions
+    case SellOrder(s) =>
+      sell enqueue SellOrderValue(s)
+      self ! MatchTransactions
+    case MatchTransactions =>
+      println(s"Matching $product: $buy $sell")
   }
 }
+
 
 object OrderBookActor {
   sealed trait BookCommand
@@ -44,4 +43,19 @@ object OrderBookActor {
   case object MatchTransactions            extends BookCommand
   case class BuyOrder(po: PositionOrder)   extends BookCommand
   case class SellOrder(po: PositionOrder)  extends BookCommand
+}
+
+case class SellOrderValue(order: PositionOrder) extends AnyVal
+case class BuyOrderValue(order: PositionOrder)  extends AnyVal
+
+object BuyOrderValue {
+  val priceDescTimestampAscOrdering = Ordering.by[BuyOrderValue, (Int, Long)] {
+    case buy => (buy.order.getDetails.getPrice, -1 * buy.order.getTimestamp)
+  }
+}
+
+object SellOrderValue {
+  val priceAscTimestampAscOrdering = Ordering.by[SellOrderValue, (Int, Long)] {
+    case sell => (-1 * sell.order.getDetails.getPrice, -1 * sell.order.getTimestamp)
+  }
 }
