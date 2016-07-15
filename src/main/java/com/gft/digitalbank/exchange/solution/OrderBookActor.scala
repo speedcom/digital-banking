92cf14bcd75d60e1b78e37307c36c6d58e44172a
speedcom <mateusz.maciaszekhpc@gmail.com>
2016-07-15 10:21:20 +0200

    matching algorithm

--- src/main/java/com/gft/digitalbank/exchange/solution/OrderBookActor.scala
+++ src/main/java/com/gft/digitalbank/exchange/solution/OrderBookActor.scala
@@ -2,15 +2,26 @@ package com.gft.digitalbank.exchange.solution
 
 import akka.actor.{Actor, ActorRef}
 import com.gft.digitalbank.exchange.model.orders.PositionOrder
-import com.gft.digitalbank.exchange.model.{OrderBook, Transaction}
+import com.gft.digitalbank.exchange.model.{OrderBook, OrderDetails, Transaction}
+import com.google.common.collect.Sets
 
 import scala.collection.mutable
+
+object OrderBookActor {
+  sealed trait BookCommand
+  case object GetTransactions              extends BookCommand
+  case object MatchTransactions            extends BookCommand
+  case class BuyOrder(po: PositionOrder)   extends BookCommand
+  case class SellOrder(po: PositionOrder)  extends BookCommand
+}
 
 class OrderBookActor(exchangeActorRef: ActorRef, product: String) extends Actor {
   import OrderBookActor._
 
   private val buy  = new mutable.PriorityQueue[BuyOrderValue]()(BuyOrderValue.priceDescTimestampAscOrdering)
   private val sell = new mutable.PriorityQueue[SellOrderValue]()(SellOrderValue.priceAscTimestampAscOrdering)
+
+  private val transactions = Sets.newHashSet[Transaction]()
 
   override def receive: Receive = {
     case GetTransactions =>
@@ -32,21 +43,72 @@ class OrderBookActor(exchangeActorRef: ActorRef, product: String) extends Actor 
       sell enqueue SellOrderValue(s)
       self ! MatchTransactions
     case MatchTransactions =>
-      println(s"Matching $product: $buy $sell")
+      for {
+        b <- buy .headOption
+        s <- sell.headOption
+        if b.price >= s.price
+        amountLimit = math.max(b.amount, s.amount)
+        priceLimit  = if(b.timestamp > s.timestamp) b.price else s.price
+        transaction = buildTransaction(b, s, amountLimit, priceLimit)
+      } yield {
+        transactions.add(transaction)
+
+        buy .dequeue()
+        sell.dequeue()
+
+        if(b.amount > amountLimit) {
+          buy.enqueue(b.ccopy(amountLimit))
+          self ! MatchTransactions
+        }
+
+        if(s.amount > amountLimit) {
+          sell.enqueue(s.ccopy(amountLimit))
+          self ! MatchTransactions
+        }
+      }
+  }
+
+  private def buildTransaction(b: BuyOrderValue, s: SellOrderValue, amountLimit: Int, priceLimit: Int) = {
+    Transaction.builder()
+      .id(transactions.size() + 1)
+      .amount(amountLimit)
+      .price(priceLimit)
+      .brokerBuy(b.order.getBroker)
+      .brokerSell(s.order.getBroker)
+      .clientBuy(b.order.getClient)
+      .clientSell(s.order.getClient)
+      .product(product)
+      .build()
   }
 }
 
+sealed trait OrderValue {
+  def order: PositionOrder
 
-object OrderBookActor {
-  sealed trait BookCommand
-  case object GetTransactions              extends BookCommand
-  case object MatchTransactions            extends BookCommand
-  case class BuyOrder(po: PositionOrder)   extends BookCommand
-  case class SellOrder(po: PositionOrder)  extends BookCommand
+  val price: Int      = order.getDetails.getPrice
+  val amount: Int     = order.getDetails.getAmount
+  val timestamp: Long = order.getTimestamp
+
+  val partiallyExecuted: Boolean
+
+  protected def update(decreaseByAmountLimit: Int): PositionOrder = {
+    PositionOrder.builder()
+      .details(new OrderDetails(order.getDetails.getAmount - decreaseByAmountLimit, order.getDetails.getPrice))
+      .timestamp(order.getTimestamp)
+      .product(order.getProduct)
+      .broker(order.getBroker)
+      .client(order.getClient)
+      .side(order.getSide)
+      .id(order.getId)
+      .build()
+  }
 }
-
-case class SellOrderValue(order: PositionOrder) extends AnyVal
-case class BuyOrderValue(order: PositionOrder)  extends AnyVal
+case class SellOrderValue(order: PositionOrder, partiallyExecuted: Boolean = false) extends OrderValue {
+  def ccopy(decreaseByAmountLimit: Int) = this.copy(order = update(decreaseByAmountLimit), true)
+}
+case class BuyOrderValue(order: PositionOrder, partiallyExecuted: Boolean = false) extends OrderValue {
+  def ccopy(decreaseByAmountLimit: Int) = this.copy(order = update(decreaseByAmountLimit), true)
+}
 
 object BuyOrderValue {
   val priceDescTimestampAscOrdering = Ordering.by[BuyOrderValue, (Int, Long)] {
