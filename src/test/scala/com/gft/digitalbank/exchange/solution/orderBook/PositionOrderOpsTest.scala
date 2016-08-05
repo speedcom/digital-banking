e5e073a2884d35e5014c9985d994895f788d77da
speedcom <mateusz.maciaszekhpc@gmail.com>
2016-08-05 14:14:20 +0200

    position-order-ops unit-tests: "change order's: timestamp, broker, price and amount taken from modification order"

--- src/test/scala/com/gft/digitalbank/exchange/solution/orderBook/PositionOrderOpsTest.scala
+++ src/test/scala/com/gft/digitalbank/exchange/solution/orderBook/PositionOrderOpsTest.scala
@@ -1,7 +1,7 @@
 package com.gft.digitalbank.exchange.solution.orderBook
 
 import com.gft.digitalbank.exchange.model.OrderDetails
-import com.gft.digitalbank.exchange.model.orders.{PositionOrder, Side}
+import com.gft.digitalbank.exchange.model.orders.{ModificationOrder, PositionOrder, Side}
 import org.scalatest.{FlatSpec, Matchers}
 
 class PositionOrderOpsTest extends FlatSpec with Matchers {
@@ -17,15 +17,40 @@ class PositionOrderOpsTest extends FlatSpec with Matchers {
     val modifiedOrder = order.minusAmount(AmountLimit(999))
 
     // then
-    modifiedOrder.getId        shouldBe order.getId
-    modifiedOrder.getTimestamp shouldBe order.getTimestamp
-    modifiedOrder.getBroker    shouldBe order.getBroker
-    modifiedOrder.getClient    shouldBe order.getClient
-    modifiedOrder.getProduct   shouldBe order.getProduct
-    modifiedOrder.getSide      shouldBe order.getSide
-
+    modifiedOrder.getId                shouldBe order.getId
+    modifiedOrder.getTimestamp         shouldBe order.getTimestamp
+    modifiedOrder.getBroker            shouldBe order.getBroker
+    modifiedOrder.getClient            shouldBe order.getClient
+    modifiedOrder.getProduct           shouldBe order.getProduct
+    modifiedOrder.getSide              shouldBe order.getSide
     modifiedOrder.getDetails.getPrice  shouldBe order.getDetails.getPrice
     modifiedOrder.getDetails.getAmount shouldBe 0
+  }
+
+  it should "change order's: timestamp, broker, price and amount taken from modification order" in {
+    // given
+    val modificationOrder = ModificationOrder.builder()
+      .id(2)
+      .broker("broker-2")
+      .details(OrderDetails.builder().amount(100).price(1000).build())
+      .modifiedOrderId(1)
+      .timestamp(1)
+      .build()
+
+    val order = orderWithAmount(10)
+
+    // when
+    val modifiedOrder = order.updateVia(modificationOrder)
+
+    // then
+    modifiedOrder.getId                shouldBe order.getId
+    modifiedOrder.getTimestamp         shouldBe modificationOrder.getTimestamp
+    modifiedOrder.getBroker            shouldBe modificationOrder.getBroker
+    modifiedOrder.getClient            shouldBe order.getClient
+    modifiedOrder.getProduct           shouldBe order.getProduct
+    modifiedOrder.getSide              shouldBe order.getSide
+    modifiedOrder.getDetails.getPrice  shouldBe modificationOrder.getDetails.getPrice
+    modifiedOrder.getDetails.getAmount shouldBe modificationOrder.getDetails.getAmount
   }
 
   private def orderWithAmount(amount: Int) = {
