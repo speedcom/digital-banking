b45c17f0311e8cf353fb38078727afe51a63121a
speedcom <mateusz.maciaszekhpc@gmail.com>
2016-08-05 12:46:56 +0200

    unit-test: "produce empty transaction set and empty final order-book when nothing was processed"

---
+++ src/test/scala/com/gft/digitalbank/exchange/solution/orderBook/MutableOrderBookTest.scala
@@ -0,0 +1,24 @@
+package com.gft.digitalbank.exchange.solution.orderBook
+
+import org.scalatest.{FlatSpec, Matchers}
+
+class MutableOrderBookTest extends FlatSpec with Matchers {
+
+  behavior of "Mutable Order Book"
+
+  it should "produce empty transaction set and empty final order-book when nothing was processed" in {
+    // given
+    val googleOrderBook = new MutableOrderBook("GOOG")
+
+    // when
+    val finalTransactions = googleOrderBook.getTransactions
+    val finalOrderBook    = googleOrderBook.getOrderBook
+
+    // then
+    finalTransactions.transactions.size() shouldBe 0
+    finalOrderBook.getBuyEntries.size     shouldBe 0
+    finalOrderBook.getSellEntries.size    shouldBe 0
+  }
+
+
+}
