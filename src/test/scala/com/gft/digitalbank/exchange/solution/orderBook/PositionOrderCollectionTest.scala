a1b0c4ecf60ca3bda3bb5249f75ad8573a6eedfb
speedcom <mateusz.maciaszekhpc@gmail.com>
2016-08-07 14:25:23 +0200

    sell-orders unit-tests: "NOT remove corresponding Order when asking for peek"

--- src/test/scala/com/gft/digitalbank/exchange/solution/orderBook/PositionOrderCollectionTest.scala
+++ src/test/scala/com/gft/digitalbank/exchange/solution/orderBook/PositionOrderCollectionTest.scala
@@ -10,26 +10,36 @@ class PositionOrderCollectionTest extends FlatSpec with Matchers {
 
   it should "remove Order when its polled" in {
     val collection = new FakePositionOrderCollection()
-
-    collection.add(
-      PositionOrder.builder()
-        .id(1)
-        .timestamp(1)
-        .broker("1")
-        .product("GOOG")
-        .side(Side.BUY)
-        .client("101")
-        .details(OrderDetails.builder().amount(1).price(1).build())
-        .build()
-    )
+    val order = randomPositionOrder
+    collection.add(order)
 
     collection.nonEmpty shouldBe true
-
     collection.poll()
+    collection.isEmpty  shouldBe true
+  }
 
-    collection.isEmpty shouldBe true
+  it should "NOT remove corresponding Order when asking for peek" in {
+    val collection = new FakePositionOrderCollection()
+    val order = randomPositionOrder
+    collection.add(order)
+
+    collection.nonEmpty shouldBe true
+    collection.peekOpt  should not be empty
+    collection.nonEmpty shouldBe true
   }
 
   private final class FakePositionOrderCollection extends PositionOrderCollection(null)
+
+  private def randomPositionOrder = {
+    PositionOrder.builder()
+      .id(1)
+      .timestamp(1)
+      .broker("1")
+      .product("GOOG")
+      .side(Side.BUY)
+      .client("101")
+      .details(OrderDetails.builder().amount(1).price(1).build())
+      .build()
+  }
 
 }
